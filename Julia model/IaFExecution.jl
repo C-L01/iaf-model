59c5a274c4efd49971e45c20dd8132c1cefe925a
C-L01 <88712229+C-L01@users.noreply.github.com>
2023-04-10 21:19:24 +0200

    Replace ODE function with a working one

--- Julia model/IaFExecution.jl
+++ Julia model/IaFExecution.jl
@@ -1,30 +1,40 @@
-include("IaFMechanics.jl")
-using .IaFMechanics, Parameters, DifferentialEquations, ParameterizedFunctions, Plots
+if isdefined(@__MODULE__, :IaFMechanics)
+    IaFMechanics isa Module || error("IaFMechanics is present and it is not a Module")
+else
+    includet("IaFMechanics.jl")
+end
+
+
+using .IaFMechanics, Parameters, DifferentialEquations, ParameterizedFunctions, LinearAlgebra, Plots
 
 
 #(A) External stimulus
-I = t -> 5
-# I(t) = exp(t/10)
+Iext = t -> 6
+# Iext(t) = exp(t/10)
 
 # TODO: external pulses should be implemented as an integration event; make sure to tstop at the pulse time
 # tPulse = 1                     #(s) time of pulse
 # dt = 1e-2                      #(s) time interval of pulse input
 # dV = 5.5                         #(V) voltage increase due to pulse
-# I(t) = (dV/dt) * (heaviside(t-tPulse) - heaviside(t-tPulse-dt))
+# Iext(t) = (dV/dt) * (heaviside(t-tPulse) - heaviside(t-tPulse-dt))
 
 
 # Exponential driving force
 fExp = @ode_def ExponentialIaF begin
-    du = (-(u - V_rest) + delta_T * exp((u - theta_rh) / delta_T) + R*I(t)) / tau
+    du = (-(u - V_rest) + delta_T * exp((u - theta_rh) / delta_T) + R*Iext(t)) / tau
 end V_rest delta_T theta_rh R tau
 
+# Aleaky = 
+
 # Leaky driving force
-fLeaky = @ode_def LeakyIaF begin
-    du = (-(u - V_rest) + R*I(t)) / tau
-end V_rest R tau
+function fLeaky(du, u, p, t)
+    # du = (-(u - V_rest) + R*Iext(t)) / tau
+    @. du = ( -(u - p[1]) + p[2]*Iext(t) ) / p[3]
+    # mul!(du, UniformScaling(-1/tau), u)
+end
 
 
-function solveiaf(u0, tStart, tEnd, params::DrivingForceParameters)
+function solveiaf(u0::Vector{Float64}, tStart::Real, tEnd::Real, params::DrivingForceParameters)
     # @unpack f, p, options = params
     @unpack V_rest, R, tau = params
 
@@ -40,7 +50,7 @@ end
 params = DrivingForceParameters{Float64}()
 u0 = generate_u0(3)         # uniformly spaced around 0
 
-sol = solveiaf(u0, 0, 5, params)
+sol = solveiaf(u0, 0, 20, params)
 
-plot(sol, idxs = [1])
+plot(sol)
 # plot(sol.t, sol.u[1])
