9554b0a687200a6596a2be72759fd6830ec8f80b
C-L01 <88712229+C-L01@users.noreply.github.com>
2023-04-21 12:55:56 +0200

    Add saving of plots
    Way of parameter setting is temporary, needs to be modularized

--- Julia model/IaFExecution.jl
+++ Julia model/IaFExecution.jl
@@ -1,19 +1,35 @@
 includet("IaFMechanics.jl")
 
-using Parameters, DifferentialEquations, Plots, Statistics, LaTeXStrings
+using Parameters, DifferentialEquations, Plots, Statistics, LaTeXStrings, Printf
 using .IaFMechanics
 
 ### Solving
 
-# Get parameters
-params = IaFParameters{Float64}(Iext=(t -> 6), N=100, w0=-1)
+Wbal = false
+leaky = true
+
+# Set parameters
+Iext = leaky ? (t -> 6) : (t -> 4.75)
+V_F = leaky ? 5 : 7
+
+r = 3                         # TODO: maybe put in params struct, especially if solveiaf is revived
+tstart, tend = 0, 30
+
+params = IaFParameters{Float64}(Iext=Iext, V_F=V_F, N=5000, w0=-1)
+
 @unpack N = params
 
-u0 = genu0(3, params)         # uniformly spaced with some radius
 
-tstart, tend = 0, 40
 
-prob = ODEProblem(genfleaky(params), u0, (tstart,tend), params)
+if Wbal
+    params = IaFParameters(params, W=(params.w0 / N) * hcat(-ones(N, N÷2), ones(N, N÷2)))   # TODO: does not work for uneven N
+end
+
+u0 = genu0(r, params)         # uniformly spaced with some radius
+
+f = leaky ? genfleaky(params) : genfexp(params)
+
+prob = ODEProblem(f, u0, (tstart,tend), params)
 sol = solve(prob; reltol=1e-4, abstol=1e-6, callback=gencallback(N), dense=false)
 
 println("System solved")
@@ -31,20 +47,33 @@ pu = plot(sol, idxs=[round(Int, i) for i in range(1, N, length=3)],
 
 pv = plot(sol.t, var.(sol.u), title="Variance", ylabel=L"Var$(t)$ (V$^2$)")
 
-pa = histogram(params.spikes.t, bins=tstart:0.1:tend, weights=1/N * params.spikes.cnt, normalize=:density,
+pa = histogram(params.spikes.t, bins=tstart:0.2:tend, weights=1/N*params.spikes.cnt, normalize=:density,
                 title="Activity", xlabel=L"$t$ (s)", ylabel=L"$A(t)$ (#/s)")
 
-plot(pu, pv, pa, layout=(3, 1), plot_title="Metrics for $N coupled neurons", link=:x, legend=false, thickness_scaling=1)
+# Combine plots of potential (u), variance (v) and activity (a)
+uvaplot = plot(pu, pv, pa, layout=(3, 1), link=:x, legend=false, thickness_scaling=1)
 
-# png("uplot")
 
-numframes = 500
-numbins::Int = (params.V_F - params.V_R)
+### Animating
 
-densityanim = @animate for t in range(tstart, tend, numframes)
-    histogram(sol(t), bins=numbins, normalize=:density,         # TODO: find out if there is a more appropriate normalization, maybe scale by N
-                xlims=(params.V_R, params.V_F), ylims=(0,40), label="t = $(round(t, digits=2))", legend=:topleft)
+fps = 15
+bins = range(params.V_R, params.V_F, step=1)
+# bins = :rice
+
+udensityanim = @animate for t in range(tstart, tend, fps*(tend-tstart))
+    histogram(sol(t), bins=bins, normalize=:pdf,
+                xlims=(params.V_R, params.V_F), ylims=(0,1), title="Potential density at t = $(@sprintf("%.2f", t))", legend=false)          
 end
 
-gif(densityanim, fps=numframes/(tend-tstart))
-# gif(densityanim, "densityanim.avi", fps=numframes/(tend-tstart))
+
+### Saving
+     
+# Assumes Iext is constant
+filesuffix = "$(leaky ? "leaky" : "exp");N=$(params.N);w0=$(params.w0);$(Wbal ? "Wbal;" : "")I0=$(params.Iext(0));r=$r;tend=$tend"
+
+png(uvaplot, "images/uva_" * filesuffix)
+
+gif(udensityanim, "animations/udensity_" * filesuffix * ".avi", fps=fps)
+
+display(uvaplot)
+gif(udensityanim, fps=fps)
