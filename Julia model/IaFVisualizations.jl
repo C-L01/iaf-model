d14def6f7f1668d8fb776a9186f72a4114678e57
C-L01 <88712229+C-L01@users.noreply.github.com>
2023-05-12 14:20:08 +0200

    Incorporate sigmas into filesuffix

--- Julia model/IaFVisualizations.jl
+++ Julia model/IaFVisualizations.jl
@@ -18,8 +18,9 @@ default(legend=false)
 Generate file suffix used for saving plots. Assumes Iext is constant.
 """
 function genfilesuffix(para)
-    @unpack leaky, N, w0, Iext, tend, r, u0distr, wdistr = para
-    return "$(leaky ? "leaky" : "exp");N=$N;w0=$w0;I0=$(Iext(0));u0distr=$u0distr;r=$r;wdistr=$wdistr;tend=$tend"
+    @unpack leaky, N, w0, Iext, tend, r, u0distr, wdistr, sig1, sig2 = para
+    includesigmas = (wdistr != :constant)       # the sigmas are only relevant with non-constant weights
+    return "$(leaky ? "leaky" : "exp");N=$N;w0=$w0;I0=$(Iext(0));u0distr=$u0distr;r=$r;wdistr=$wdistr;$(includesigmas ? "sig1=$sig1;sig2=$sig2;" : "")tend=$tend"
     # $(Wbal ? "Wbal;" : "")
 end
 
